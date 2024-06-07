f4ad323156fe9267c1b0e98884da47819429683f
gpaulero <169163764+gpaulero@users.noreply.github.com>
2024-06-07 17:19:15 -0300

    Create main.py

---
+++ aplicacion/main.py
@@ -0,0 +1,11 @@
+# main.py
+
+from menu import mostrar_menu_principal
+
+def main():
+    print("Bienvenido al Gestor de Tareas Personales")
+    while True:
+        mostrar_menu_principal()
+
+if __name__ == "__main__":
+    main()
