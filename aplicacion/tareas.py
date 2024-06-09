0d8a5d3a67986b68fe57733a391a44490175acb0
gpaulero <169163764+gpaulero@users.noreply.github.com>
2024-06-09 15:24:30 -0300

    Create tareas.py

---
+++ aplicacion/tareas.py
@@ -0,0 +1,13 @@
+# tareas.py
+
+def crear_tarea():
+    print("Opción seleccionada: Crear una nueva tarea")
+
+def ver_tareas():
+    print("Opción seleccionada: Ver todas las tareas")
+
+def editar_tarea():
+    print("Opción seleccionada: Editar una tarea")
+
+def eliminar_tarea():
+    print("Opción seleccionada: Eliminar una tarea")
